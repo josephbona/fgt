62bdab8ceead036b52f1dcac1e8e52144b9d756d
josephbona <joseph.bona07@gmail.com>
2023-04-03 19:10:08 -0400

    Footer and collection

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -7,37 +7,11 @@ import { buttonVariants } from "@/components/ui/button"
 
 export function SiteFooter() {
   return (
-    <footer>
-      <div className="container flex h-16 items-center justify-between">
-        <div className="flex">
-          <Link href="/">
-            <Image
-              className="h-8 w-auto"
-              src="/logo.svg"
-              width="562"
-              height="132"
-              alt="Fast Growing Trees"
-            />
-            <span className="sr-only">{siteConfig.name}</span>
-          </Link>
-        </div>
-        <div className="justify-end">
-          <nav className="flex items-center space-x-1">
-            <Link href="/cart">
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                  className: "text-[#005745]",
-                })}
-              >
-                <ShoppingCart className="h-5 w-5" />
-                <span className="sr-only">Cart</span>
-              </div>
-            </Link>
-          </nav>
-        </div>
-      </div>
+    <footer className="bg-[#005745] py-6 border-t border-green-700">
+      <p className="text-xs text-green-100 text-center">
+        &copy; {new Date().getFullYear()} {siteConfig.name}. All rights
+        reserved.
+      </p>
     </footer>
   )
 }
